8d58ef900c82369917074666860b7de5e037e04c
Olena Prosiatnikova <margulay1976@gmail.com>
2024-12-18 18:13:34 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,19 @@
+//Напиши функцію slugify(title), яка приймає заголовок статті,
+//параметр title і повертає slug, створений із цього рядка.
 
+//Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+//Усі символи slug повинні бути в нижньому регістрі.
+//Усі слова slug повинні бути розділені тире.
+ 
+function slugify(title) {
+   return title
+        .toLowerCase()                     
+        .replace(/[^a-z0-9\s-]/g, '')       
+        .replace(/\s+/g, '-')               
+        .replace(/-+/g, '-')               
+        .replace(/^[-]+|[-]+$/g, '');  
+}
+console.log(slugify("Arrays for beginners")); 
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
